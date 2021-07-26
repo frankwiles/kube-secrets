c8376f8f79ae7abf350c994a528f38cb1ef96958
Frank Wiles <frank@revsys.com>
2021-07-25 21:48:42 -0600

    Initial sorta working version

---
+++ src/main.rs
@@ -0,0 +1,84 @@
+use clap::{App, Arg, ArgMatches};
+use kube::{Client, Api};
+use k8s_openapi::api::core::v1::Secret;
+
+#[derive(Debug)]
+struct Config {
+    show_all: bool,
+    namespace: String,
+    query: String,
+}
+
+fn determine_config(matches: ArgMatches) -> Config {
+    let show_all = matches.is_present("all");
+    let namespace = matches.value_of("namespace").unwrap().to_string();
+    let mut query = String::from("");
+
+    if matches.is_present("query") {
+        query = matches.value_of("query").unwrap().to_string();
+    }
+
+    let config = Config{
+        show_all: show_all,
+        namespace: namespace,
+        query: query,
+    };
+
+    return config
+}
+
+fn display_secret(c: &Config, s: &Secret) -> bool {
+    if c.show_all || s.type_.as_ref().unwrap() == "Opaque" {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let matches = App::new("secrets")
+        .version("0.1.0")
+        .author("Frank Wiles <frank@revsys.com>")
+        .about("List commonly needed secrets in a decoded way")
+        .arg(
+            Arg::new("all")
+                .about("Show all secret types")
+                .short('a')
+                .long("all"),
+        )
+        .arg(
+            Arg::new("namespace")
+                .about("Namespace to list secrets from")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::new("query")
+                .about("Match secrets names by substring")
+                .takes_value(true)
+        )
+        .get_matches();
+
+    // Turn command line matches into our Config struct
+    let config = determine_config(matches);
+    // println!("{:?}", config);
+
+    let client = Client::try_default().await?;
+    let secrets: Api<Secret> = Api::namespaced(client, &config.namespace);
+
+    for s in secrets.list(&Default::default()).await? {
+        let display = display_secret(&config, &s);
+        if !display { continue; }
+        println!("{}:", s.metadata.name.unwrap());
+
+        for (key, value) in s.data {
+            let bstring = std::str::from_utf8(&value.0).unwrap();
+            println!("  {}: {}", key, bstring);
+        }
+
+    }
+
+    Ok(())
+}
