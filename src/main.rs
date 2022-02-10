dc9e2ee1422e1554ceb5c3f74758860eacffce59
Frank Wiles <frank@revsys.com>
2022-02-10 14:19:29 -0600

    Fix clap 3.0.0-beta issues by upgrading and moving to derive

--- src/main.rs
+++ src/main.rs
@@ -3,46 +3,31 @@ extern crate colorful;
 extern crate k8s_openapi;
 extern crate kube;
 
-use clap::{App, Arg, ArgMatches};
+use clap::Parser;
 use colorful::Colorful;
 use k8s_openapi::api::core::v1::{Namespace, Secret};
 use kube::{Api, Client};
 
-#[derive(Debug)]
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
 struct Config {
+    #[clap(short='a', long)]
     show_all: bool,
+
     namespace: String,
-    query: String,
-}
-
-fn determine_config(matches: ArgMatches) -> Config {
-    let show_all = matches.is_present("all");
-    let namespace = matches.value_of("namespace").unwrap().to_string();
-    let mut query = String::from("");
-
-    if matches.is_present("query") {
-        query = matches.value_of("query").unwrap().to_string();
-    }
-
-    let config = Config {
-        show_all: show_all,
-        namespace: namespace,
-        query: query,
-    };
-
-    return config;
+    query: Option<String>,
 }
 
 fn display_secret(c: &Config, s: &Secret) -> bool {
     if c.show_all || s.type_.as_ref().unwrap() == "Opaque" {
-        // We should show the secret if we aren't querying
-        if c.query.is_empty() {
-            return true;
-        }
+        let query = match &c.query {
+            Some(q) => q,
+            None => return true
+        };
 
         // Filter the name against our query
         let secret_name = s.metadata.name.as_ref().unwrap();
-        if secret_name.contains(&c.query) {
+        if secret_name.contains(query) {
             return true;
         }
         return false;
@@ -53,31 +38,7 @@ fn display_secret(c: &Config, s: &Secret) -> bool {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let matches = App::new("secrets")
-        .version("0.1.0")
-        .author("Frank Wiles <frank@revsys.com>")
-        .about("List commonly needed secrets in a decoded way")
-        .arg(
-            Arg::new("all")
-                .about("Show all secret types")
-                .short('a')
-                .long("all"),
-        )
-        .arg(
-            Arg::new("namespace")
-                .about("Namespace to list secrets from")
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::new("query")
-                .about("Match secrets names by substring")
-                .takes_value(true),
-        )
-        .get_matches();
-
-    // Turn command line matches into our Config struct
-    let config = determine_config(matches);
+    let config = Config::parse();
 
     let client = Client::try_default().await?;
     let secrets: Api<Secret> = Api::namespaced(client, &config.namespace);
